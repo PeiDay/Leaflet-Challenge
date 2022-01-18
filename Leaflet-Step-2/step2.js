62c7c4f50403f6b456b986f848c092b27a93d542
PeiDay <91394158+PeiDay@users.noreply.github.com>
2022-01-17 20:26:29 -0500

    step2

--- Leaflet-Step-2/step2.js
+++ Leaflet-Step-2/step2.js
@@ -1,17 +1,53 @@
-console.log("step 1 JS read in")
+console.log("step 2 JS read")
 
 // Store our API endpoint as queryUrl.
-// monthly data
-// var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-// weekly data 
-var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+// ** monthly data
+// var earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+// ** weekly data 
+var earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 
-// ****************************************** //
-d3.json(queryUrl).then(function (data) {
-    // Once we get a response, send the data.features object to the createMap function.
-    createMap(data.features);
-  });
-// ****************************************** //
+// tectonic plates url
+var tecPlateUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+
+var theMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// Create the base layer
+var myMap = L.map("map", {
+    center: [38.50, -96.00],
+    zoom: 3,
+    layers: [theMap, topo]
+});
+
+theMap.addTo(myMap)
+
+var baseMap = {
+   "Basic Map": theMap ,
+   Topography: topo
+};
+
+// variable for the layers
+var earthquakesLayer = new L.LayerGroup();
+var tecPlateLayer = new L.LayerGroup();
+
+// Overlays that can be toggled on or off
+var overlayMaps = {
+    Earthquakes: earthquakesLayer,
+    "Tectonic Plates": tecPlateLayer,
+  };
+
+// Pass our map layers into our layer control.
+// Add the layer control to the map.
+L.control
+.layers(baseMap, overlayMaps)
+.addTo(myMap);
+  
 
 // Define a markerSize based on earthquake magnitude
 function markerSize(magnitude) {
@@ -21,53 +57,37 @@ function markerSize(magnitude) {
 // Define markerColor based on earthquake depth
 function markerColor(depth) {
     if (depth <= 10) {
-        return "rgb(253,212,158)"
+        return "#bcbddc"
     } else if (depth <= 30) {
-        return "rgb(253,187,132)"
+        return "#9e9ac8"
     } else if (depth <= 50) {
-        return "rgb(252,141,89)"
+        return "#807dba"
     } else if (depth <= 70) {
-        return "rgb(239,101,72)"
+        return "#6a51a3"
     } else if (depth <= 90) {
-        return "rgb(215,48,31)"
+        return "#54278f"
     } else {
-        return "rgb(127,0,0)"
+        return "#3f007d"
     }
 };
 
-function createMap (earthquakeData){
-    // Create the base layer
-    var myMap = L.map("map", {
-        center: [
-            38.50, -96.00
-        ],
-        zoom: 5,
-    });
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
-
-
+d3.json(earthquakeUrl).then(function (data) {
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
     // Run the onEachFeature function once for each piece of data in the array.
-    L.geoJSON(earthquakeData, {
+    L.geoJSON(data, {
         pointToLayer: function circleLayer(features, latlng) {
-
             return L.circleMarker(latlng, {
                 radius: markerSize(features.properties.mag),
                 fillColor: markerColor(features.geometry.coordinates[2]),
+                stroke: true,   
                 color: "white",
                 weight: 0.7,
                 opacity: 0.8,
                 fillOpacity: 0.7
-        
             });
-        
         },
         onEachFeature: onEachFeature
-    }).addTo(myMap);
-
+    }).addTo(earthquakesLayer);
 
     // Define a function to run once for each feature in the features array.
     // Give each feature a popup that describes the magnitude, place, and time of the earthquake.
@@ -103,10 +123,17 @@ function createMap (earthquakeData){
         return legend_div;
     };
     legend.addTo(myMap);
+});
 
+d3.json(tecPlateUrl, function(tectonicData) {
+    L.geoJSON(tectonicData, {
+        color: "red",
+        weight: 3,
+})
+.addTo(tecPlateLayer);
 
-};
-
+tecPlateLayer.addTo(myMap);
+});
 
 
 
